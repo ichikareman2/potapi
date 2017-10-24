48168773040a85a290e4951fec29856f72caad25
kevin <willardquing@gmail.com>
2017-10-25 04:47:54 +0800

    browse endpoint for browsing directory

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ let formidable = require('formidable')
 let fs = require('fs')
 let cors = require('cors')
 
+let util = require('util')
 
+let readdir = util.promisify(fs.readdir)
+// let stat = util.promisify(fs.stat)
 
 let app = express();
 //config
@@ -17,20 +20,20 @@ app.post('/upload', (req, res) => {
     form.multiples = true;
     // form.uploadDir = path.join(__dirname, '/uploads');
 
-    let destinationCreated = new Promise((res,rej) => {
+    let destinationCreated = new Promise((res, rej) => {
         fs.mkdir(destination, (err) => {
-            if(err) {
-                if(err.code === 'EEXIST') {
+            if (err) {
+                if (err.code === 'EEXIST') {
                     res();
                 }
-                else{
+                else {
                     rej();
                 }
             }
             res()
         })
     })
-    
+
     form.uploadDir = destination;
     form.on('file', (field, file) => {
         fs.rename(file.path, path.join(form.uploadDir, file.name), (err) => {
@@ -56,15 +59,76 @@ app.post('/upload', (req, res) => {
 
 app.get('/browse', (req, res) => {
     let destination = conf.uploadDestination;
-    fs.readdir(destination, (err, files) => {
-        if (err) {
-            console.error(`An error has occured: \n ${err}`);
-        }
-        else {
-            res.send(files)
-        }
+
+
+    let dir = readdir(destination);
+    dir.then((dirFiles) => {
+        let dirStatPromises = dirFiles.map(x => {
+            return stat(path.join(destination, x))
+        })
+        Promise.all(dirStatPromises).then(dirStats => {
+            res.send(dirStats)
+        })
+    }).catch(err => {
+        console.error(err)
     })
+
+    // // let dirStat = dir.map(x => {
+    // //     let st = stat(path.join(destination, x))
+    // // })
+    // // let stats = Promise.all(dirStat)
+    // // res.send(stats)
+    // readdir(destination).then((files) => {
+    //     let stats = files.map((x, i) => {
+    //         let filepath = path.join(destination, x)
+    //         stat(filepath).then((fileStat) => {
+
+
+    //             fileStat.isFile()
+    //         })
+    //     })
+    // })
+
+
+
+    // fs.readdir(destination, (err, files) => {
+    //     let dirData = files.map((x, i) => {
+    //         let filepath = path.join(destination, x)
+    //         fs.stat(filepath, (err, y) => {
+
+    //         })
+    //     })
+    //     if (err) {
+    //         console.error(`An error has occured: \n ${err}`);
+    //     }
+    //     else {
+    //         res.send(dirData)
+    //     }
+    // })
 })
+
+function stat(filePath) {
+    return new Promise((res, rej) => {
+        fs.stat(filePath, (err, stat) => {
+            if (err) rej(err);
+            else res({ 
+                isFile: stat.isFile(), 
+                size: stat.size === 0 ? stat.size : stat.size / 1024,
+                file: path.basename(filePath), 
+                path: filePath })
+        })
+    })
+
+}
+
+// function readdir (dir) {
+//     return new Promise((res,rej) => {
+//         fs.readdir(dir, (err, files) => {
+//             if(err) rej(err);
+//             else res(files)
+//         })
+//     })
+// }
 
 app.listen(3000, () => {
     console.log('Server listens to 3000')
